e90a891a64d563c157447eadc6f6d5130559e78d
Arian <arian.kulmer@web.de>
2026-06-07 07:54:49 +0000

    peridot: qcril_database: Add migration to disable redir_party_num Caused incoming caller number not being parsed correctly by Phone app on VoLTE and VoWiFi. No idea why Xiaomi enabled this ancient prop.
    Change-Id: I5a9c30c3f0e42c7816d3dc7eebb73e97cdc0fed8
    Co-Authored-By: Andrzej Perczak <linux@andrzejperczak.com>

--- qcril-database/config/0015.1_config.sql
+++ qcril-database/config/0015.1_config.sql
@@ -6,3 +6,4 @@
 CREATE TABLE IF NOT EXISTS qcril_properties_table (property TEXT PRIMARY KEY NOT NULL, def_val TEXT, value TEXT);
 INSERT OR REPLACE INTO qcril_properties_table(property, def_val) VALUES('qcrildb_version',15.1);
 UPDATE qcril_properties_table SET def_val="0" WHERE property="persist.vendor.radio.poweron_opt";
+UPDATE qcril_properties_table SET def_val="false" WHERE property="persist.vendor.radio.redir_party_num";
